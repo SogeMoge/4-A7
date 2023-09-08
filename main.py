275b141a5e0d3f644ddd27d4dcfce4ef8374e8e2
SogeMoge <valterob@gmail.com>
2023-09-08 12:47:53 +0400

    populate description with multiline

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 # https://stackoverflow.com/a/65908383
 # fixes libgcc_s.so.1 must be installed for pthread_cancel to work
 import ctypes
-
 libgcc_s = ctypes.CDLL("libgcc_s.so.1")
 
 # module for importing env variables
@@ -24,10 +23,6 @@ import requests
 
 # pycord modules
 import discord
-
-# from discord.ext import commands
-# from discord.utils import get
-# from discord.commands import Option
 
 # from discord.commands import permissions
 from discord.ui import Button, View
@@ -112,25 +107,27 @@ async def on_message(message):
 
     xws_string = json.dumps(xws_raw.json())
     xws_dict = json.loads(xws_string)
+    
+    squad_list = {}
 
     embed = discord.Embed(
         title=xws_dict['name'],
         colour=discord.Colour.random(),
         url=message.content,
-        description=xws_dict['faction'],
+        description=xws_dict['faction'] + "[" + xws_dict['points'] + "]" + "\n" + xws_dict['pilots'][0] + "\n" + xws_dict['pilots'][2],
     )
 
-    embed.add_field(
-        name=discord.Embed.Empty,
-        value=xws_dict['points'],
-        inline=True,
-    )
+    # embed.add_field(
+    #     name=discord.Embed.Empty,
+    #     value=xws_dict['points'],
+    #     inline=True,
+    # )
 
-    embed.add_field(
-        name=discord.Embed.Empty,
-        value=xws_dict['pilots'],
-        inline=True,
-    )
+    # embed.add_field(
+    #     name=discord.Embed.Empty,
+    #     value=xws_dict['pilots'],
+    #     inline=True,
+    # )
 
     # for i, ember_heare in enumerate(xws_dict)
 
@@ -141,122 +138,6 @@ async def on_message(message):
 
     await yasb_channel.send(embed=embed)
     await message.delete()
-
-
-# @bot.event
-# async def on_message(message):
-#     """parse legacy-yasb link to post embed list"""
-#     if message.author.bot:  # check that author is not the bot itself
-#         return
-
-#     if "://xwing-legacy.com/?f" in message.content:
-#         yasb_channel = message.channel
-
-#         # convert YASB link to XWS
-#         yasb_link = message.content
-#         # print(f"1) yasb_link = {yasb_link}")
-#         yasb_convert = yasb_link.replace(
-#             "://xwing-legacy.com/",
-#             "://squad2xws.herokuapp.com/yasb/xws",
-#         )
-#         # print(f"2) yasb_convert = {yasb_convert}")
-#         yasb_xws = requests.get(yasb_convert, timeout=10)
-#         # print(f"3) yasb_xws = {yasb_xws}")
-#         #############
-#         # don't know if it works at all???
-#         # yasb_xws = unescape(yasb_xws) # delete all characters which prevents proper parsing
-
-#         yasb_json = yasb_xws.json()  # raw XWS in JSON
-#         # print(f"4) yasb_json = {yasb_json}")
-#         yasb_json = json.dumps(
-#             yasb_json
-#         )  # convert single quotes to double quotes
-#         # print(f"5) yasb_json = {yasb_json}")
-#         yasb_dict = json.loads(
-#             yasb_json
-#         )  # convert JSON to python object
-#         # print(f"6) yasb_dict = {yasb_dict}")
-#         #############
-#         for (
-#             key,
-#             value,
-#         ) in (
-#             yasb_dict.items()
-#         ):  # add embed title with list name as hyperlink
-#             if key in ["name"]:
-#                 embed = discord.Embed(
-#                     title=value,
-#                     colour=discord.Colour.random(),
-#                     url=message.content,
-#                     description="YASB Legacy 2.0 list",
-#                 )
-#         try:  # use custom name for squads with default name from yasb
-#             embed
-#         except NameError:
-#             embed = discord.Embed(
-#                 title="Infamous Squadron",
-#                 colour=discord.Colour.random(),
-#                 url=message.content,
-#                 description="YASB Legacy 2.0 list",
-#             )
-
-#         embed.set_footer(
-#             text=message.author.display_name,
-#             icon_url=message.author.display_avatar,
-#         )
-
-#         ####### TO DO ######## compare parsed results to data in xwing-data manifest
-#         # # get JSON manifest from ttt-xwing-overlay repo
-#         # manifest_link = requests.get(BASE_URL + MANIFEST)
-#         # manifest = manifest_link.json()
-
-#         # files = (
-#         #     manifest['damagedecks'] +
-#         #     manifest['upgrades'] +
-#         #     [manifest['conditions']] +
-#         #     [ship for faction in manifest['pilots']
-#         #         for ship in faction['ships']]
-#         # )
-
-#         # _data = {}
-#         # loop = asyncio.get_event_loop()
-#         # # get JSON manifest from ttt-xwing-overlay repo
-#         ####### TO DO ######## compare parsed results to data in xwing-data manifest
-
-#     for (
-#         key,
-#         value,
-#     ) in (
-#         yasb_dict.items()
-#     ):  # add embed fields with faction and list name
-#         if key in ["faction"]:
-#             embed.add_field(name=key, value=value, inline=True)
-
-#     pilots_total = len(yasb_dict["pilots"])
-
-#     for pilot in range(
-#         pilots_total
-#     ):  # add embed fields for each pilot in a list
-#         embed.add_field(
-#             name=yasb_dict["pilots"][pilot]["id"],
-#             # value=list(yasb_dict["pilots"][pilot]["upgrades"].values()),
-#             value=re.sub(
-#                 r"[\[\]\']",
-#                 "\u200b",
-#                 str(
-#                     list(
-#                         yasb_dict["pilots"][pilot]["upgrades"].values()
-#                     )
-#                 ),
-#             ),
-#             inline=False,
-#         )
-#     await yasb_channel.send(embed=embed)
-#     await message.delete()
-
-
-# http://xwing-legacy.com/ -> http://squad2xws.herokuapp.com/yasb/xws
-# http://xwing-legacy.com/?f=Separatist%20Alliance&d=v8ZsZ200Z305X115WW207W229Y356X456W248Y542XW470WW367WY542XW470WW367W&sn=Royal%20escort&obs=
 
 #  #########################
 # INFO COMMANDS
